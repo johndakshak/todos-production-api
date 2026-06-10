3b10b86d3c7b2cc890634112ffad39607d13eaf1
johndakshak <johndakshak@gmail.com>
2026-06-10 20:35:05 +0100

    Update CORS middleware

--- app/main.py
+++ app/main.py
@@ -45,6 +45,7 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://localhost:5173",
 ]
 
 app.mount("/static", StaticFiles(directory="/app/static"), name="static")
