d782e62987c1f01adf1c6f71cfd6be3eb198a0aa
johndakshak <johndakshak@gmail.com>
2026-06-10 20:45:44 +0100

    Update CORS middleware

--- app/main.py
+++ app/main.py
@@ -38,15 +38,24 @@ origins = [
     "http://localhost:8000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-]
-
-origins = [
-    os.getenv("FRONTEND_URL", "http://localhost:3000"),
-    "http://localhost:8000",
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
     "http://localhost:5173",
 ]
+
+# origins = [
+#     os.getenv("FRONTEND_URL", "http://localhost:3000"),
+#     "http://localhost:8000",
+#     "http://127.0.0.1:5500",
+#     "http://localhost:5500",
+#     "http://localhost:5173",
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="/app/static"), name="static")
 @app.get('/')
